c5c57ce4b7ff3136c0395b3a8ed83af2692a71e3
Kandr9 <kandr13579@gmail.com>
2023-06-30 16:30:19 +0300

    15

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// 15
+System.Console.WriteLine("Введите от 1 до 7");
+int n =Convert.ToInt32(Console.ReadLine());
+
+if(n>=1 && n<=7)
+{
+    if (n==6  | n==7) 
+    System.Console.WriteLine("Сегодня выходной");
+    else
+    System.Console.WriteLine("Сегодня рабочий день");
+}
+else
+System.Console.WriteLine("Неверное число");
